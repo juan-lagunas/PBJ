b4e4f29be11cf8a560f1d2d3c0ddee67f126c89d
Juan Lagunas <juanlagunas28@gmail.com>
2023-05-13 23:55:32 -0400

    Finish inventory page layout and create models.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Category, Part, Inventory, Logs
 # Register your models here.
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('id', 'category')
+
+class AdminPart(admin.ModelAdmin):
+    list_display = ('id', 'partname', 'category', 'descrition', 'price', 'user', 'date')
+
+class AdminInventory(admin.ModelAdmin):
+    list_display = ('id', 'partname', 'category', 'price', 'quantity',)
+    
+class AdminLogs(admin.ModelAdmin):
+    list_display = ('id', 'partname', 'category', 'price', 'quantity', 'user', 'date')
+
+
+
+
+admin.site.register(Category)
+admin.site.register(Part)
+admin.site.register(Inventory)
+admin.site.register(Logs)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+    
+    def __self__(self):
+        return f'{self.id}: {self.category}'
+    
+class Part(models.Model):
+    partname = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    description = models.CharField(max_length=100)
+    price = models.FloatField()
+    user = models.CharField(max_length=40)
+    date = models.DateField()
+
+    def __self__(self):
+        return f'{self.partname} ({self.category}) - {self.description} : ${self.price}'
+    
+class Inventory(models.Model):
+    partname = models.CharField(max_length=100)
+    category = models.CharField(max_length=64)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+    @property
+    def total(self):
+        value = self.price * self.quantity
+        return f'{value:,.2f}'
+    
+    @property
+    def usd(self):
+        return f'{self.price:,.2f}'
+
+    def __self__(self):
+        return f'{self.partname} ({self.category}) - Qty: {self.quantity} Price: ${self.price} Total: %{self.total}'
+    
+class Logs(models.Model):
+    partname = models.CharField(max_length=100)
+    category = models.CharField(max_length=64)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    user = models.CharField(max_length=40)
+    date = models.DateTimeField()
+
+    @property
+    def usd(self):
+        value = self.price * self.quantity
+        return f'{value:,.2f}'
+    
+    def __self__(self):
+        return f'{self.partname} ({self.category}) - Qty: {self.quantity} Price: ${self.price} User: {self.user} Date: {self.date}'
